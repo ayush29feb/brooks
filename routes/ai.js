15f9750721974736f94d1c7982a3484ac4c3469f
Ayush Saraf <ayush29f@uw.edu>
2017-02-21 10:08:55 -0800

    fixing the hook

--- routes/ai.js
+++ routes/ai.js
@@ -3,14 +3,8 @@ var router = express.Router();
 
 // fullfillment post request
 router.post('/', function(req, res, next) {
-    console.log(req.body);
-    res.json({
-        "fulfillment": {
-            "speech": "Today in Boston: Fair, the temperature is 37 F",
-            "source": "apiai-weather-webhook-sample",
-            "displayText": "Today in Boston: Fair, the temperature is 37 F"
-        }
-    });
+    console.log(req.body.result.parameters);
+    res.json(req.body.result.fulfillment);
 });
 
 module.exports = router;
