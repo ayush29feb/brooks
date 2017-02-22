96f8628939dbc8536382c558c207c3ff060b132a
Ayush Saraf <ayush29f@uw.edu>
2017-02-22 15:43:56 -0800

    added cards for property stuff

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,30 @@ var admin = require("firebase-admin");
 var db = admin.database();
 var ref = db.ref("server/maintenance");
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+/* GET home page. */
+router.get('/dashboard', function(req, res, next) {
   ref.on('value', function(snapshot){
     var data = snapshot.val();
-    res.render('index', { title: 'Property Pro' , data: data});
+    res.render('dashboard', { title: 'Property Pro' , data: data});
   })
-  
+});
+
+router.get('/maintenance', function(req, res, next) {
+  ref.on('value', function(snapshot){
+    var data = snapshot.val();
+    res.render('maintenance', { title: 'Property Pro' , data: data});
+  });
+});
+
+router.get('/reports', function(req, res, next) {
+  ref.on('value', function(snapshot){
+    var data = snapshot.val();
+    res.render('reports', { title: 'Property Pro' , data: data});
+  });
 });
 
 module.exports = router;
